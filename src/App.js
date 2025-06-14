641281f84b5cee663107f590c2f34bc93b835e3c
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-14 23:55:21 +0100

    Create Wrapper component with content, title, year, tags, tag, description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React from "react";
+import { GlobalStyled } from "./GlobalStyled.js";
 import { Container } from "./common/Container";
+import {
+  Content,
+  Description,
+  Tag,
+  Tags,
+  Title,
+  Wrapper,
+  Year,
+} from "./common/Wrapper";
+import PosterSVG from "./components/Poster.jsx";
 
 function App() {
   return (
-    <Container>
-      <div> cos tam jest </div>
-    </Container>
+    <>
+      <GlobalStyled />
+      <Container>
+        <Wrapper>
+          <PosterSVG />
+          <Content>
+            <Title>Mulan</Title>
+            <Year>2020</Year>
+            <Tags>
+              <Tag>Action</Tag>
+              <Tag>Adventure</Tag>
+              <Tag>Drama</Tag>
+            </Tags>
+            <Description>
+              A young Chinese maiden disguises herself as a male warrior in
+              order to save her father. Disguises herself as a male warrior in
+              order to save her father. A young Chinese maiden disguises herself
+              as a male warrior in order to save her father.
+            </Description>
+          </Content>
+        </Wrapper>
+      </Container>
+    </>
   );
 }
 
