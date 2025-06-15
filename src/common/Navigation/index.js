a65b4276ef31b57f55d0298eabefbdee6d372a6c
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-15 21:15:17 +0100

    Feature: Create Navigation component with initial structure

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+<<<<<<< HEAD
 import { List, Item, StyledNav } from "./styled";
 
 const Navigation = () => {
@@ -17,6 +18,19 @@ const Navigation = () => {
         </Item>
       </List>
     </StyledNav>
+=======
+import { StyledNavLink, List, Item } from "./styled";
+
+const Navigation = () => {
+  return (
+    <Navigation>
+      <List>
+        <Item>
+          <NavLink></NavLink>
+        </Item>
+      </List>
+    </Navigation>
+>>>>>>> 6e3b7bf (Feature: Create Navigation component with initial structure)
   );
 };
 
