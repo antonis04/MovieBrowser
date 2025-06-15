50af296920d3e8d0c4da73c192c1db382d34e06f
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-15 21:14:35 +0100

    Feature: Reintroduce React Router and Redux store to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import Navigation from "./common/Navigation/index.js";
+<<<<<<< HEAD
 
 function App() {
   return (
@@ -15,6 +16,35 @@ function App() {
       </Router>
     </Provider>
   );
+=======
+import PeoplePage from "./features/people/PeoplePage.js";
+import MovieList from "./features/movies/MovieList.js";
+import MoviePage from "./features/movies/MoviePage.js";
+import PeopleList from "./features/people/PeopleList.js";
+
+function App() {
+  <Provider store={store}>
+    <Navigation />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="">
+          <MoviePage />
+        </Route>
+        <Route path="">
+          <PeoplePage />
+        </Route>
+        <Route path="/">
+          <MovieList />
+        </Route>
+        <Route path="/">
+          <PeopleList />
+        </Route>
+      </Routes>
+    </Router>
+    ;
+  </Provider>;
+>>>>>>> 2a46e30 (Feature: Reintroduce React Router and Redux store to App)
 }
 
 export default App;
