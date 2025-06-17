9b5ddfd1e32c9e12a2a530e7e9b3cb4d2ee348cb
Agnieszka Szajnowska <agnieszka.badyla@o2.pl>
2025-06-17 07:27:43 +0200

    Loading, NoResults and Error pages added to folder Common

---
+++ src/common-2/NoResults/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { ReactComponent as Icon } from "../../images/NoResults.svg"
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 40px 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding: 10px 0;
+  }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const NoResultsIcon = styled(Icon)`
+    width: 100%;
+    height: 100%;
+
+    path {
+    stroke-width: 2.39px;
+  }
+`;
