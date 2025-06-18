08ced2b77dc2b441a07b44c602688f60d7074cea
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-18 20:56:34 +0100

    delete comment

--- src/features/people/PeoplePage.js
+++ src/features/people/PeoplePage.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 function PeoplePage() {
   return <div>PeoplePage</div>;
 }
-=======
-function PeoplePage() {}
->>>>>>> 4d643de (feat: add PeoplePage component skeleton)
 
 export default PeoplePage;
