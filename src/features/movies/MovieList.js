e38c1ee8e601671eb23accc593d1b48e389a77d0
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-18 20:56:16 +0100

    feat(MovieList): implement handleClick to fetch movies from API

--- src/features/movies/MovieList.js
+++ src/features/movies/MovieList.js
@@ -1,5 +1,19 @@
-function MovieList() {
-  return <div>MovieList</div>;
-}
+import { fetchPopularMovies } from "../../api/api";
 
-export default MovieList;
+export default () => {
+  const handleClick = async () => {
+    try {
+      const movies = await fetchPopularMovies();
+      console.log(movies);
+    } catch (error) {
+      console.error("Błąd pobierania filmów:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Movie Page</h1>
+      <button onClick={handleClick}>Pobierz filmy</button>
+    </>
+  );
+};
