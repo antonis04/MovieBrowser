ee8c81b66f368d6da11067687224985e220bce44
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-21 21:17:24 +0100

    Create Logo, LogoText and new styled for them

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,13 +1,48 @@
 import styled from "styled-components";
 
 export const StyledNav = styled.nav`
-  background: ${({ theme }) => theme.color.lightgrey};
-  color: ${({ theme }) => theme.color.black};
-  width: 1920px;
+  background: ${({ theme }) => theme.color.black};
+  color: ${({ theme }) => theme.color.white};
+  width: 100%;
   height: 94px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
+
+    &:hover {
+      color: ${({ theme }) => theme.color.grey};
+    }
+  }
+  svg {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+export const LogoText = styled.span`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 40px;
+  letter-spacing: -1.5px;
+  text-transform: capitalize;
+  margin-left: 12px;
+`;
+
+export const NavMenu = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
 `;
 
 export const List = styled.ul`
@@ -20,4 +55,13 @@ export const List = styled.ul`
 
 export const Item = styled.li`
   font-weight: 600;
+  list-style: none;
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
+
+    &:hover {
+      color: ${({ theme }) => theme.color.grey};
+    }
+  }
 `;
