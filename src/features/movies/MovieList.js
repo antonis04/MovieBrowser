55e3c854f030ec7fec747d02ed2ef9898542571b
Wioletta Koperska <koperskawioletta@gmail.com>
2025-06-26 21:41:49 +0100

    refactor(MovieList): major structure overhaul and styling improvements

--- src/features/movies/MovieList.js
+++ src/features/movies/MovieList.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { fetchPopularMovies } from "../../api/api";
+import { Link } from "react-router-dom";
+import { GlobalStyle } from "../../GlobalStyle";
+import { Container } from "../../common/Container/styled";
+import {
+  Cast,
+  CastRow,
+  Name,
+  Picture,
+  PersonTitle,
+  Actor,
+} from "../../common/Cast/styled";
+import { Strong, Title } from "../../common/Wrapper/styled";
+import { ReactComponent as EmptyPicture } from "../../images/EmptyPicture.svg";
 
-export default function MovieList() {
+const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
 
@@ -26,21 +39,49 @@ export default function MovieList() {
       setError("Error fetching movies!");
     }
   };
+  console.log({ Cast, CastRow, Name, Picture, PersonTitle });
 
   return (
-    <div>
-      <h1>Movie List</h1>
-      <button onClick={handleClick}>Pobierz filmy</button>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Title>Popular Movies</Title>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <strong>{movie.title}</strong> ({movie.release_date})
-          </li>
-        ))}
-      </ul>
-    </div>
+        <Cast>
+          <CastRow>
+            {movies.map((movie) => (
+              <Link
+                to={`/movie/${movie.id}`}
+                key={movie.id}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <PersonTitle>
+                  {movie.poster_path ? (
+                    <Picture
+                      src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  ) : (
+                    <EmptyPicture width={177} height={264} />
+                  )}
+                  <Name>
+                    <Actor>{movie.title}</Actor>
+                    <Strong>
+                      {movie.release_date
+                        ? movie.release_date.slice(0, 4)
+                        : "Unknown"}
+                    </Strong>
+                  </Name>
+                </PersonTitle>
+              </Link>
+            ))}
+          </CastRow>
+        </Cast>
+      </Container>
+    </>
   );
-}
+};
+
+export default MovieList;
