ea7c9722813a846e16f76d26cc095fb358143e97
Antek <antoni.samol@gmail.com>
2025-07-04 14:33:07 +0200

    Picture object changed to movie poster

--- src/features/movies/MoviePage.js
+++ src/features/movies/MoviePage.js
@@ -31,6 +31,7 @@ import {
   Strong,
   Summary,
   Votes,
+  MoviePoster,
 } from "../../common/Wrapper/styled.js";
 import StarSVG from "../../components/StarSVG/StarSVG.js";
 import {
@@ -163,14 +164,13 @@ const MoviePage = () => {
       <Container>
         <Wrapper>
           {movie.poster_path ? (
-            <Picture
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            <MoviePoster
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
-              style={{ borderRadius: "12px" }}
             />
           ) : (
             <EmptyPictureWrapper small>
-              <EmptyPicture width={200} height={300} />
+              <EmptyPicture width={312} height={464} />
             </EmptyPictureWrapper>
           )}
           <Content>
